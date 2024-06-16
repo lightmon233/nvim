21f26a6e039ff43dffbd2510b8700b16e1e800c0
Lightmon <lightmon5210@outlook.com>
2024-06-16 22:15:53 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -13,6 +13,10 @@
 #include <cstdio>
 #include <cmath>
 #include <ctime>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <chrono>
+#include <iomanip>
 
 // #pragma O(2)
 // #define int long long
@@ -25,6 +29,7 @@
 // lg(8) = 3, lg(16) = 4, 计算x的以2为底的对数的向下取整, 即x的最高位1的位置
 #define lg(x) __lg(x)
 
+using namespace __gnu_pbds;
 using namespace std;
 
 using LL = long long;
@@ -33,27 +38,35 @@ using PLL = pair<LL, LL>;
 using PDI = pair<double, int>;
 using PDD = pair<double, double>;
 using ULL = unsigned long long;
+template<class T> using sset = tree<T, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>;
 
 const int mod = 998244353;
 const int N = 1e5 + 10, M = 2e5 + 10;
-const int INF = (1LL << 31) - 1;
-const LL LLF = 2e18 - 1;
+const int INF = 0x3f3f3f3f;
+const LL LLF = 0x3f3f3f3f3f3f3f3f;
 
 // 用于检测程序占用内存大小
 bool Mbe;
 
 // 随机数生成器
-// mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+// mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
 mt19937_64 rnd(1064);
 int rd(int l, int r) {return rnd() % (r - l + 1) + l;}
+double rdd(double l, double r) {return rnd() * (r - l) / rnd.max() + l;}
 
 // 取模运算
-void addt(int &x, int y) {x += y, x >= mod && (x -= mod);}
+void plut(int &x, int y) {x += y, x >= mod && (x -= mod);}
 void subt(int &x, int y) {x -= y, x < 0 && (x += mod);}
 void mult(int &x, int y) {x = (LL)x * y % mod;}
-int add(int x, int y) {return x + y >= mod ? x + y - mod : x + y;}
+int plu(int x, int y) {return x + y >= mod ? x + y - mod : x + y;}
 int sub(int x, int y) {return x - y < 0 ? x - y + mod : x - y;}
 int mul(int x, int y) {return (LL)x * y % mod;}
+
+// 判断大小
+template<typename T, typename U>
+void chkmin(T &a, U b) {if (a > b) a = b;}
+template<typename T, typename U>
+void chkmax(T &a, U b) {if (a < b) a = b;}
 
 // 快速幂
 int qpow(int a, int b) {
@@ -81,17 +94,26 @@ int bsearch(int l, int r, function<bool(int)> f) {
     return l;
 }
 
+void setIO() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+}
+
 // 读入优化
-inline int read() {
-    int s = 0, w = 1;
-    char c = getchar();
-    while (c < 48 || c > 57) {
-        if (c == '-') w = -1;
-        c = getchar();
-    }
-    while (c >= 48 && c <= 57)
-        s = (s << 1) + (s << 3) + c - 48, c = getchar();
-    return s * w;
+template<typename T>
+inline T read(){
+	T a = 0;
+    bool s = 0;
+	char ch = getchar();
+	while (ch > '9' || ch < '0'){
+		if (ch == '-') s ^= 1;
+		ch = getchar();
+	}
+	while (ch >= '0' && ch <= '9'){
+		a = (a << 3) + (a << 1) + (ch ^ 48);
+		ch = getchar();
+	}
+	return s ? -a : a;
 }
 
 // 输出优化
@@ -101,20 +123,20 @@ inline void pf(LL x) {
     putchar(x % 10 + 48);
 }
 
+/*
 // 求组合数
-// const int Z = 1e6 + 10;
-// int fc[Z], ifc[Z];
-// int bin(int n, int m) {
-//     if(n < m) return 0;
-//     return 1ll * fc[n] * ifc[m] % mod * ifc[n - m] % mod;
-// }
-// void init_fac(int Z) {
-//     for(int i = fc[0] = 1; i < Z; i++) fc[i] = 1ll * fc[i - 1] * i % mod;
-//     ifc[Z - 1] = qpow(fc[Z - 1], mod - 2);
-//     for(int i = Z - 2; ~i; i--) ifc[i] = 1ll * ifc[i + 1] * (i + 1) % mod;
-// }
-
-/* template above */
+const int Z = 1e6 + 10;
+int fc[Z], ifc[Z];
+int bin(int n, int m) {
+    if (n < m) return 0;
+    return 1ll * fc[n] * ifc[m] % mod * ifc[n - m] % mod;
+}
+void init_fac(int Z) {
+    for (int i = fc[0] = 1; i < Z; i++) fc[i] = 1ll * fc[i - 1] * i % mod;
+    ifc[Z - 1] = qpow(fc[Z - 1], mod - 2);
+    for (int i = Z - 2; ~i; i--) ifc[i] = 1ll * ifc[i + 1] * (i + 1) % mod;
+}
+*/
 
 void solve() {
 
@@ -123,7 +145,7 @@ void solve() {
 bool Med;
 int main() {
     fprintf(stderr, "%.3lf MB\n", (&Med - &Mbe) / 1048576.0);
-    // ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    // setIO();
     int T = 1;
     cin >> T;
     while (T --) solve();
