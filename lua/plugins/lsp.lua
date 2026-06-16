58307b7512903176c2fdd83397a8f61be6aace2f
lightmon <lightmon5210@outlook.com>
2026-06-16 13:57:37 +0800

    added vtsls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,7 @@ require("mason").setup({
 require("mason-lspconfig").setup({
   -- 确保安装，根据需要填写
   ensure_installed = {
-    "lua_ls", "clangd", "bashls"
+    "lua_ls", "clangd", "bashls", "vtsls"
   },
 })
 
@@ -20,4 +20,5 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('clangd')
 vim.lsp.enable('bashls')
+vim.lsp.enable('vtsls')
 
